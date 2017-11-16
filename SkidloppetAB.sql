51dddcb582a9a52c9b32346187c9cfdf13892dac
ELjungdahl <33284569+ELjungdahl@users.noreply.github.com>
2017-11-16 14:41:37 +0100

    Add files via upload

--- SkidloppetAB.sql
+++ SkidloppetAB.sql
@@ -7,15 +7,16 @@ CREATE TABLE report( #DELSTRÄCKA!
 	#rID INT auto_increment,
     rMail VARCHAR(30),
     rName VARCHAR(30), # "R" i Rname står för report
-    #descr TINYTEXT, #Tinytext limiterar tecknen till max 255. 
-    #probChoice VARCHAR(30), #Problemval
+    rDescr TINYTEXT, #Beskriving om var problemet skedde #Tinytext limiterar tecknen till max 255. 
+    probChoice VARCHAR(30), #Problemval
+	rOrt VARCHAR(20),
     
 	PRIMARY KEY (rMail)
     
 )engine=innodb;
 
-insert into report (rMail, rName) value ('mail@mail.com', 'Kurt');
-select * from report;
+insert into report (rMail, rName, rDescr, probChoice, rOrt) value ('mail@mail.com', 'Kurt', 'Död björn ligger på spåret', '', '');
+
 
 CREATE TABLE news(
 	
@@ -23,7 +24,7 @@ CREATE TABLE news(
     nDate DATE,
     article TINYTEXT,
     nHeader VARCHAR(20),
-    nPhase VARCHAR(30), #etapp
+    nOrt VARCHAR(30), #etapp
     
     PRIMARY KEY (nID)
     
@@ -32,59 +33,49 @@ CREATE TABLE news(
 CREATE TABLE feedback(
 
 	fbID INT auto_increment,
-    fb int(1),
+    fb int(1), #1-4 i nöjdhetsgrad
     fbComment TINYTEXT,
     
     PRIMARY KEY (fbID)
     
 )engine=innodb;
     
-CREATE TABLE distance ( #sträcka
 
-	nr INT(2), #Delsträcka 1-21
-    ort VARCHAR(20),
-    dStatus INT(1), #DStatus = Distance status (1=Öppen/0=Stängd/2=Under arbete)
-	
-	PRIMARY KEY (nr) 
-    
-)engine=innodb;
-
-INSERT INTO distance (nr, ort, dStatus) VALUE ('21','Norrhyttan', '1');
     
 CREATE TABLE workOrder( #arbetsorder
 
-	workOrderID INT auto_increment, 
-	startDate DATE,
-	endDate DATE,
-    typ INT(1),  #1 = Under förhandling / 2 = Pågående / 3 = Genomförd
+	wWorkOrderID INT auto_increment, 
+	wStartDate DATE,
+	wEndDate DATE,
+    wTyp INT(1),  #1 = Under förhandling / 2 = Pågående / 3 = Genomförd
     
-    dNr INT(2), #distance number
+    WdNr INT(2), #Delsträckenummer
     
-    PRIMARY KEY (workOrderID),
+    PRIMARY KEY (wWorkOrderID)
     
-    FOREIGN KEY (dNr) REFERENCES distance(nr)
+    
          
 )engine=innodb;
      
-INSERT INTO workOrder (workOrderID,startDate,endDate,typ,dNr) VALUE ('','2017-06-15','2017-06-15', '1', '21');
+INSERT INTO workOrder (wWorkOrderID,wStartDate,wEndDate,wTyp,WdNr) VALUE ('','2017-06-15','2017-06-15', '1', '21');
      
      
 CREATE TABLE anv( #Användare
     
-    pnr CHAR(12),
-    passw VARCHAR(20),
-    usern VARCHAR(20),
+    aPnr CHAR(12),
+    aPassw VARCHAR(20),
+    aUsern VARCHAR(20),
     aNamn VARCHAR(20),
     aMail VARCHAR(20),
-    adress VARCHAR(30),
-    tel INT(10),
+    aAdress VARCHAR(30),
+    aTel INT(10),
     aTyp INT(1),
     
-    PRIMARY KEY (pnr)
+    PRIMARY KEY (aPnr)
     
 )engine=innodb;    
 
-INSERT INTO anv (pnr) VALUE ('199403023634');
+INSERT INTO anv (aPnr) VALUE ('199403023634');
 
 CREATE TABLE UndEntArb(
 
@@ -92,7 +83,7 @@ CREATE TABLE UndEntArb(
 	woID INT auto_increment, #WordingOrderID
     
     FOREIGN KEY(aPnr) REFERENCES anv(pnr),
-    FOREIGN KEY(woID) REFERENCES workOrder(workOrderID)
+    FOREIGN KEY(woID) REFERENCES workOrder(wWorkOrderID)
 
 )engine=innodb;    
 
@@ -108,6 +99,25 @@ CREATE TABLE inaccessible(
 
 INSERT INTO inaccessible (iPnr, iStart, iEnd) VALUE ('199403023634','2017-06-15','2017-06-15');
 
+CREATE TABLE orter(
+
+	oID INT (1),
+	oOrt VARCHAR(20),
+    oStatus VARCHAR (15),
+    oInfo TINYTEXT, #TINYTEXT limiterar texten till ca 255 tecken
+    oDate date,
+    
+	PRIMARY KEY (oID)
+
+)engine=innodb;
+
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('1', 'Hedemora', 	'Öppen', '', '');
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('2', 'Norrhyttan', 	'Öppen', '', '');
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('3', 'Bondhyttan', 	'Öppen', '', '');
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('4', 'Bomannbo', 	'Stängd', 'Översvämmat spår', '2018-01-14');
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('5', 'Smedjebacken', 'Under arbete', 'Träd över stigen', '2017-12-21');
+INSERT INTO orter (oID, oOrt, oStatus, oInfo, oDate) VALUE ('6', 'Björsjö', 'öppen', '', '');
+
 
 
 DELIMITER //
@@ -119,5 +129,5 @@ CREATE PROCEDURE Antalordrar(IN typval INT(1))
  END //
 DELIMITER ;
 
-CALL AntalOrdrar('1');
+#CALL AntalOrdrar('1');
     
